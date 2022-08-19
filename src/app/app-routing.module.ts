1ae9c253d43ab5d0f170f1072ee2b195b629a0a3
Bader <baderbenlhachemi@gmail.com>
2022-08-19 10:28:49 +0100

    Working on Company module front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CompaniesListComponent} from "./views/pages/company/companies-list/companies-list.component";
+import {CompanyDetailsComponent} from "./views/pages/company/company-details/company-details.component";
+import {AddCompanyComponent} from "./views/pages/company/add-company/add-company.component";
+import {EditCompanyComponent} from "./views/pages/company/edit-company/edit-company.component";
+import {NotfoundComponent} from "./views/pages/notfound/notfound.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',redirectTo: '/companies', pathMatch: 'full' },
+  { path: 'companies',component:CompaniesListComponent},
+  { path: 'company/:company_id',component:CompanyDetailsComponent},
+  { path: 'add-company',component:AddCompanyComponent},
+  { path: 'edit-company/:company_id',component:EditCompanyComponent},
+  { path: 'delete-company/:company_id',component:CompanyDetailsComponent},
+  { path: '**',component:NotfoundComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CompaniesListComponent } from './views/pages/company/companies-list/companies-list.component';
+import { AddCompanyComponent } from './views/pages/company/add-company/add-company.component';
+import { CompanyDetailsComponent } from './views/pages/company/company-details/company-details.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
+import { EditCompanyComponent } from './views/pages/company/edit-company/edit-company.component';
+import { NotfoundComponent } from './views/pages/notfound/notfound.component';
+import { ConfirmationPopoverModule} from "angular-confirmation-popover";
 
+// @ts-ignore
+// @ts-ignore
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompaniesListComponent,
+    AddCompanyComponent,
+    CompanyDetailsComponent,
+    EditCompanyComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ConfirmationPopoverModule.forRoot({
+      confirmButtonType: 'danger'
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
